4d9e5d229128f43634168c3890db887592414569
Peradat Permjai <gullwings@msn.com>
2018-02-02 18:22:28 +0700

    status
    status pudate

--- GPIO.py
+++ GPIO.py
@@ -1,8 +1,38 @@
+
 import RPi.GPIO as GPIO ## Import GPIO library
 import ConfigParser
 import os
 import time
 import requests
+import sys
+import socket
+import serial
+import fcntl
+import struct
+
+REMOTE_SERVER = "www.google.com"
+
+
+def internet_on():
+    try:
+        # see if we can resolve the host name -- tells us if there is
+        # a DNS listening
+        host = socket.gethostbyname(REMOTE_SERVER)
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        s = socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 def SendAlartFun(channel):
     try:
@@ -10,6 +40,62 @@ def SendAlartFun(channel):
         print ('content     ' + resp.content) 
     except:
         print 'SendAlartFun Connection lost'
+      
+
+def SendStatusFun(message):
+    try:
+        ser.flushInput()
+        ser.flushOutput()
+        time.sleep(0.1)
+        ser.write('AT+QCCID\r')
+        time.sleep(2)
+
+        for num in range(0, 10):
+            bufsid = ser.readline()
+            if len(bufsid) >= 20 :
+                break
+
+        replacements = (',', '\r', '\n', '?')
+        for r in replacements:
+            bufsid = bufsid.replace(r, ' ')
+        SID = bufsid.split(" ")
+        print SID
+        #SID = CID[1]
+        
+        ser.write('AT+GSN\r')
+        time.sleep(1)
+        for num in range(0, 10):
+            bufemi = ser.readline()
+            if len(bufemi) >= 10 :
+                break
+
+        replacements = (',', '\r', '\n', '?')
+        for r in replacements:
+            bufemi = bufemi.replace(r, ' ')
+        IMEI = bufemi.split(" ")
+        print IMEI
+        #IMEI = emi[0]
+        sidOk = True
+        
+        print id
+        ip = get_ip_address('ppp0') 
+        print ip
+        print SID
+        print IMEI
+        api = nti_url.split("/")
+        print api[2]
+
+        if len(SID[1]) <= 13:
+            return False
+
+        resp = requests.get('http://188.166.197.107:8001?id={0}&ip={1}&sid={2}&imei={3}&api={4}&msg={5}'.format(id,ip,SID[1],IMEI[0],api[2],message), timeout=3.001)
+        print ('content     ' + resp.content) 
+        return True
+    except:
+        print 'SendStatusFun Connection lost'
+    return False
+
+
         
 def ConfigSectionMap(section):
     dict1 = {}
@@ -62,10 +148,48 @@ GPIO.setup(4, GPIO.IN) # Power
 
 GPIO.add_event_detect(3, GPIO.RISING, callback=SendAlartFun, bouncetime=100)
 
+sendStart = False
+
+time.sleep(5)
+ser = serial.Serial('/dev/ttyUSB2', 115200, timeout=.5)
+ser.write('AT+QGPS=1\r')
+ser.write('ATE0\r')
+time.sleep(2)
+
+current_time = time.time()
+startTime = time.time()
+timeStart = time.time()
+timeout = time.time() + 60
 while True:
+
+    current_time = time.time()
+#I/O Power on
+    if sendStart == False  :
+        if internet_on() == True :  
+            if(SendStatusFun('Power Start') == True):
+                sendStart = True
+                print sendStart
+
+#I/O Power off
     if(GPIO.input(4) == 0):
         print('Power Off')
+        SendStatusFun('Power Off {0:.1f} Min'.format((current_time - timeStart)/60))
         time.sleep(10)
+        
         os.system('sudo shutdown -h now')
         break
+
+#On line status
+    if current_time - startTime > 60*10:
+        SendStatusFun('On {0:.1f} Min'.format((current_time - timeStart)/60))
+        startTime = current_time
+
+#Ennable GPS
+    if time.time() > timeout:
+        timeout = time.time() + 60
+        ser.write('ATE0\r')
+        ser.write('AT+QGPS=1\r')
+        for num in range(0, 5):
+            bufemi = ser.readline()
+
 GPIO.cleanup()
